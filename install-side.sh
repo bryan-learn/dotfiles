db40bbfa74539f3d68575ae5788e100a99820af1
Bryan Learn <blearn@psc.edu>
2014-08-14 14:22:56 -0400

    adding my dotfiles and install scripts

---
+++ install-side.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+############################
+# install-dotfiles.sh
+# This script copies the side script to /usr/bin/
+############################
+
+# move side script to /usr/bin/
+echo "Copying side to /usr/bin/"
+cp side /usr/bin/side
+echo "...done"
+
